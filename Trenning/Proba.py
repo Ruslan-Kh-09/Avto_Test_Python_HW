8053f99e6f84c5e120577659693617302991e91d
Ruslan-kh-09 <Ruslan_skypro@mail.ru>
2026-03-27 21:54:03 +0300

    2026-03-27_21-53 (01 Коммит для тренинга)

---
+++ Trenning/Proba.py
@@ -0,0 +1,5 @@
+def boom(a,b,c):
+    result=a*b/c
+    return result
+v=boom(9,8,7)
+print(v)
---
+++ Trenning/Trening_01.py
@@ -0,0 +1,24 @@
+# my_heigh = 180
+# print(my_heigh)
+
+# my_name = "Руслан"
+# my_name = "Руслан Хатукаев"
+# print(my_name)
+
+# pet_name = input("Как зовут твоего питомца?")
+# print("Ваш любимчик - " + pet_name)
+
+#def print_python():
+#   print("Учу Python!")
+#print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('S')
+print_letter('t')
+print_letter('y')
+print_letter('d')
+print_letter('e')
+print_letter('n')
+print_letter('t')
