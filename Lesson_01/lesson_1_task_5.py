893367ea60e605151bea3f80c0743505e23d5ac1
Ruslan-kh-09 <Ruslan_skypro@mail.ru>
2026-03-27 21:05:10 +0300

    1 Коммит для первого урока

---
+++ Lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def tel_number(num):
+    print(num, end="")
+
+
+tel_number("8 ")
+tel_number("8")
+tel_number("0")
+tel_number("0 ")
+tel_number("5")
+tel_number("5")
+tel_number("5 ")
+tel_number("3")
+tel_number("5 ")
+tel_number("3")
+tel_number("5")
