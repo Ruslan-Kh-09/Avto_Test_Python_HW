efa1ce08fe22c6cef1c19df2d40438d377e339fd
Ruslan-kh-09 <Ruslan_skypro@mail.ru>
2026-04-06 00:09:15 +0300

    2026-04-06 (01 Коммит для урока_3)

---
+++ 03_Lesson/lesson_3_task_1.py
@@ -0,0 +1,8 @@
+from user import User
+
+my_user = User('Джони', 'Иванов')
+
+print('Вывод имени и фамилии пользователя')
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
---
+++ 03_Lesson/lesson_3_task_3.py
@@ -0,0 +1,28 @@
+from address import Address
+from mailing import Mailing
+
+address_to = Address("296000", "Черкесск", "Фрунзе", "2", "22")
+
+address_from = Address("369000", "Ставрополь", "Мира", "22", "71")
+
+my_mailing = Mailing(
+    to_address=address_to,
+    from_address=address_from,
+    cost=888.99,
+    track="RU123456789CN"
+)
+
+from_addr = my_mailing.from_address
+to_addr = my_mailing.to_address
+
+
+def format_address(addr):
+
+    return (f"{addr.index}, {addr.city}, {addr.street}, "
+            f"{addr.house} - {addr.apartment}")
+
+
+print(f"Отправление {my_mailing.track}"
+      f" из {format_address(from_addr)}"
+      f" в {format_address(to_addr)}."
+      f" Стоимость {my_mailing.cost} рублей.")
