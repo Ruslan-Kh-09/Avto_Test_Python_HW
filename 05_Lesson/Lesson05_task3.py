b1db203f491e4663580ea0c6f46e986a8b759904
Ruslan-kh-09 <Ruslan_skypro@mail.ru>
2026-04-16 10:02:47 +0300

    2026-04-16 (03 Коммит для урока_5 (Домашка)

--- 05_Lesson/Lesson05_task3.py
+++ 05_Lesson/Lesson05_task3.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 wait = WebDriverWait(driver, 10)
 
