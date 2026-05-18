a36bb85e5c1fb314e71ad7434a3d24dabffe84d8
Ruslan-kh-09 <Ruslan_skypro@mail.ru>
2026-05-18 07:52:27 +0300

    2026-05-18 (02 Коммит для домашки по уроку 9

--- 08_Lesson/yougile_avto_api.py
+++ 08_Lesson/yougile_avto_api.py
@@ -11,19 +11,19 @@ class YougileAvtoApi:
         }
 
     def create_project(self, title: str):
-        """[POST] Создание проекта"""
+
         url = f"{self.base_url}/api-v2/projects"
         # Передаем обязательное поле title
         payload = {"title": title}
         return requests.post(url, json=payload, headers=self.headers)
 
     def get_project(self, project_id: str):
-        """[GET] Получение проекта по ID"""
+
         url = f"{self.base_url}/api-v2/projects/{project_id}"
         return requests.get(url, headers=self.headers)
 
     def update_project(self, project_id: str, new_title: str):
-        """[PUT] Изменение проекта по ID"""
+
         url = f"{self.base_url}/api-v2/projects/{project_id}"
         payload = {"title": new_title}
         return requests.put(url, json=payload, headers=self.headers)
